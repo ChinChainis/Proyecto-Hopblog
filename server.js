15fa67c8e7c3614ca484e2fc5082bd16fe262d37
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-08-14 23:29:22 +0200

    #6 movidos archivos de renderizado de video a server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import multer from 'multer';
 import session from 'express-session';
 import app from './video.js'
 
-import {getImgIDbyId,insertarFrameID,getFramesbyId, deleteIMGID, getUsuario,insertaUsuario, insertaVideo, insertaVideo3, deleteIMG,getImgbyTitulo, getVidbyAutor, getVidbyUrl} from './funciones_sql.js';
+import {getImgIDbyId,insertarFrameID,getFramesbyId, deleteIMGID, getUsuario,insertaUsuario, insertaVideoID, insertaVideo3, deleteIMG,getImgbyTitulo, getVidbyAutor, getVidbyUrl} from './funciones_sql.js';
 
 import ffmpegStatic from 'ffmpeg-static';
 import ffmpeg from 'fluent-ffmpeg';
@@ -19,11 +19,14 @@ ffmpeg.setFfmpegPath(ffmpegStatic);
 
 
 //const app = express();
-const origin = "";
 var router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const upload = multer({dest: 'frames/'})
+const origin = "";
+
 
 //CORS
 //https://es.stackoverflow.com/questions/618761/error-de-cors-en-nodejs-con-react
@@ -60,19 +63,104 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use( express.json() ); // <== Make sure we can handle JSON data from the client
 
 
-/*
-app.get('/',(req,res) => {
-    contador = 0
-    //console.log("index cargado");
-    //res.status(200);
-    //res.sendFile(__dirname + "/" + "styles.css");
-    res.render('index');
-    //podríamos enviar variables al html si hacemos res.render('index', {text: "World"}) y luego en el html Hello <%= text %>
-    //res.sendFile(path.join(__dirname,'/index.html'));
-});*/
 
 
-//export default app;
+app.post("/frames/:id/:nombre", upload.single('file'), (req,res)=>{
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', origin);
+
+    const nom = req.params.nombre;
+    const id_ani = req.params.id;
+
+    console.log("dentro creavideo:" + id_ani);
+
+    console.log("dentro server " + req.body  ); // <== Receives: [ 'A', 42, false ]
+    var inBase64Format  = JSON.stringify(req.body )
+
+    console.log("dentro server parte principio " + inBase64Format.slice(1,2)); 
+    let numframe = inBase64Format.slice(1,2);
+
+    let base64Image = inBase64Format.split(';base64,').pop();   
+    
+    //let r = Math.floor((Math.random())*1000)+100;
+    let id_rand = Math.random() * (10000 - 1000) + 1000;
+
+    var buff = Buffer.from(base64Image).toString("base64");
+
+    const frame = insertarFrameID(id_rand,id_ani,nom, inBase64Format);
+    console.log("base de datos actualizada: " + frame);
+
+});
+
+
+
+app.get('/creavideo/:id/:nombre',async (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+
+    const foldPath = './frames2';
+
+    fs.readdir(foldPath, function(err, files) {
+    const txtFiles = files.filter(el => path.extname(el) === '.png');
+    console.log(txtFiles);
+        for (let i = 0; i < txtFiles.length; i++) {
+            let filePath = foldPath + "/" + txtFiles[i];
+            console.log(filePath);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(`Error removing file: ${err}`);
+                    return;
+                }
+
+                console.log(`File ${filePath} has been successfully removed.`);
+            });
+        }
+    });
+
+
+
+    let id_ani_prev = req.params.id;
+    const id_ani = Math.round(id_ani_prev);
+
+    const nom = req.params.nombre;
+
+    console.log("dentro creavideo:" + id_ani);
+
+    const notes = await getImgIDbyId(id_ani);
+    console.log("base de datos: " + notes.length);
+    for (let i = 0; i < notes.length; i++) {
+        let numtemp = JSON.stringify(notes[i]).split(',');
+        let numframe = numtemp[0].slice(11,);
+
+        let base64Image = JSON.stringify(notes[i]).split(';base64,').pop();
+        console.log("BUFF: " + numframe);
+        fs.writeFile('./frames2/frame'+numframe+'.png', base64Image, {encoding: 'base64'}, function(err) {
+            console.log('File created');
+        });
+    }
+
+    ffmpeg()
+
+        .input('frames2/frame%01d.png')
+        .inputOptions('-framerate', '10')
+        .videoCodec('libx264')
+        .saveToFile('public/vids/'+id_ani+'.mp4')
+        .on('progress', (progress) => {
+            if (progress.percent) {
+            console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+            }
+        })
+        .on('end', () => {
+            console.log('FFmpeg con nombre has finished.');
+            //res.redirect('/preview');            
+        })
+        .on('error', (error) => {
+            console.error(error);
+        });
+    //res.render('preenviado');
+    //res.send(notes);
+
+});
+
 
 
 app.listen(port, () => {
