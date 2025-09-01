9eb972bf4f8926136cdf76032adebac02233aadd
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-09-01 21:30:13 +0200

    #6 arreglo de redirección v3

--- public/funciones/funciones_canvas.js
+++ public/funciones/funciones_canvas.js
@@ -195,7 +195,7 @@ function envioframes(id_rand,numnote){
       //let listaframesstrings = [];
       //https://proyecto-hopblog.onrender.com/
       //"http://127.0.0.1:3000/frames/"+id_rand+"/"+numnote
-      const apiCall = (posact) => fetch("http://127.0.0.1:3000/frames/"+id_rand+"/"+numnote, {
+      const apiCall = (posact) => fetch("https://proyecto-hopblog.onrender.com/frames/"+id_rand+"/"+numnote, {
             method: "POST",
             headers: {
             'Content-Type': 'application/json'
@@ -244,13 +244,13 @@ function envioframes(id_rand,numnote){
 function renderizavid(id_rand,numnote){
    //fetch("http://127.0.0.1:3000/creavideo/"+id_rand+"/"+numnote);
    //"https://proyecto-hopblog.onrender.com/creavideo/"
-   window.location.href = "http://127.0.0.1:3000/creavideo/"+id_rand+"/"+numnote;
+   window.location.href = "https://proyecto-hopblog.onrender.com/creavideo/"+id_rand+"/"+numnote;
 }
 
 function renderizaprev(id_rand,numnote){
    spanloading.textContent = "";
 
-   window.location.href = "http://127.0.0.1:3000/preview/"+id_rand+"/"+numnote;
+   window.location.href = "https://proyecto-hopblog.onrender.com/preview/"+id_rand+"/"+numnote;
 }
 
 
